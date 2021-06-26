cc14e99b43379dc8ca52148808813af5837986b2
Loren Cipriano <loren25cipriano@gmail.com>
2021-06-26 15:16:11 -0700

    commit tree e7d613b1565af9bd9a26029724e860ee6dc99fb4

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
+  
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
-    addToy = !addToy;
+    addToy = !addToy;  // TOGGLE SYNTAX 
     if (addToy) {
       toyFormContainer.style.display = "block";
     } else {
